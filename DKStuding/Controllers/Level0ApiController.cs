91772b9575b3b8b4fe11ddb89be07f17199accc5
Данил Крапивин <danil.krapivin@gmail.com>
2022-10-16 19:02:43 +0300

    Add project files.

---
+++ DKStuding/Controllers/Level0ApiController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DKStuding.Controllers
+{
+    [ApiController]
+    [Route("Level0/Api/[action]")]
+    public class Level0ApiController : Controller
+    {
+        [HttpGet]
+        public string Index()
+        {
+            return "Index page";
+        }
+
+        [HttpGet]
+        public string Hello(string name)
+        {
+            return "Hello " + name + "!";
+        }
+
+        [HttpGet]
+        public int Abs(int x)
+        {
+            return Math.Abs(x);
+        }
+
+        [HttpGet]
+        public int Summ3(int x, int y, int z)
+        {
+            return x + y + z;
+        }
+    }
+}
---
+++ DKStuding/Controllers/Level0MvcController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DKStuding.Controllers
+{
+    public class Level0MvcController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            string model = "Index page";
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Hello(string name)
+        {
+            string model = "Hello " + name + "!";
+            return View(model);
+        }
+    }
+}
